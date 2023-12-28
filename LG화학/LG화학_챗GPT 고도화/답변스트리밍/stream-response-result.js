2e0f6218529451d5aab99573d452c597a33f96bd
sunhee47 <sunny47.jun@gmail.com>
2023-12-28 18:10:14 +0900

    highlight language 설정 및 예외사항 추가

--- LG화학/LG화학_챗GPT 고도화/답변스트리밍/stream-response-result.js
+++ LG화학/LG화학_챗GPT 고도화/답변스트리밍/stream-response-result.js
@@ -12,8 +12,18 @@ const promptInput = document.getElementById("promptInput");
 const generateBtn = document.getElementById("generateBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resultText = document.getElementById("resultText");
+const hideResult = document.getElementById("hide_result");
 
 let controller = null; // Store the AbortController instance
+
+$(document).ready(function() {
+
+  var script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js';
+  document.head.appendChild(script);
+
+  //hljs.highlightAll();
+});
 
 const generate = async () => {
   // Alert the user if no prompt value
@@ -100,7 +110,7 @@ const generate = async () => {
           
           console.log('before tiem : '+ new Date());
 
-          const wait = await printResponse(arrResult);
+          const wait = await printResponse(arrResult, no);
           console.log('wait > '+wait);
 
           console.log('after tiem : '+ new Date());
@@ -143,56 +153,115 @@ stopBtn.addEventListener("click", stop);
 
 /////////////////////////////////////////////////////////////////
 let object = {};
-//var textVal = "";
+var textVal = "";
 const cursorHtml = '<span class="blinking-cursor">●</span>';
 var row = 0;
 
-var languageArr = ["python", "java", "javascript"];     // highlight language 배열
+// 변수 초기화는 대화세션 단위로...
+var languageArr = ["python", "java", "javascript", "html", "js", "css", "자바스크립트", "파이썬", "자바"];     // highlight language 배열
+
+let languageMap = new Map([
+  ["python", "python"],
+  ["파이썬", "python"],
+  ["파이선", "python"],
+  ["javascript", "javascript"], 
+  ["js", "javascript"], 
+  ["자바스크립트", "javascript"], 
+  ["java", "java"], 
+  ["자바", "java"], 
+  ["html", "html"], 
+  ["HTML", "html"], 
+  ["css", "css"] 
+]);
+
 var codeStart = false;                                  // code start
 var codeIndex = 0, chasu = 1;                           // code Index, code chasu
 var codeLang = "";                                      // code language 
 var codeId = "";                                        // code ID
-async function printResponse(arrResult) {
+
+
+function isIncludeLanguage(cont) {
+  for (let [key, value] of languageMap) {
+    //console.log(key, value);            //"Lokesh" 37 "Raj" 35 "John" 40
+    if(cont.indexOf(key) > -1)  {
+      return value;
+    }    
+  }
+  /*
+  for(var i=0; i<languageArr.length; i++) {
+    if(cont.indexOf(languageArr[i]) > -1)  {
+      return languageArr[i];
+    }
+  }*/
+}
+
+//console.log(isIncludeLanguage('자바로 구구단 코드 출력해 줘'));
+
+async function printResponse(arrResult, no) {
 
   return new Promise(resolve => {
     
     var col = 0;
     var val = arrResult; 
     var len = val.length;
-    var textVal = "";
+    //var textVal = "";
     var bfCode = "";                                    // 코드 시작을 알리는 ``` 이 잘려서 오는 경우 앞부분.
 
     const interval = setInterval ( () => {
 
       $(".blinking-cursor").remove();
 
-      //textVal = textVal + val[col];
-      //$("#resultText").html(textVal+cursorHtml);    /// 1.
-      //  $("#resultText").empty().append(textVal);
-      if(val[col] == "```" || val[col] == "``" || $.trim(val[col]) == "`") {  // 코드 시작 또는 끝 부분인 경우.
+      if(no == 0) {
+        if(col == 0) {      
+          console.log('첫 글자 : '+val[col]);
+          
+          codeStart = false;
+          codeIndex = 0;
+          codeLang = "";
+  
+          if(val[col] == 'for' || val[col] == '//') {   // 바로 코드 시작인 경우. 
+            if(codeStart==false) {          // 코드 시작일 경우.
+              codeId = "code-result"+chasu;
+              $("#resultText").append('<pre><code id="'+codeId+'"></code></pre>');
+              hljs.highlightAll();
+              textVal = "";             // 코드 내용 초기화.
+              $("#"+codeId).removeClass("language-undefined");
+              codeStart = true;
+
+              chasu++;
+            }
+          }
+        }
+      }
+
+      if(val[col] == "```" || val[col] == "``") {  // 코드 시작 또는 끝 부분인 경우.
 
         //console.log('code ... '+val[col]+', bfCode .. '+bfCode);
-        if(bfCode == "``" || $.trim(bfCode) == "`") {     // ``` 표시가 잘려서 오는 경우. 뒷부분 처리. 
-          //console.log('bfCode = '+bfCode);
-          bfCode = "";
-        }
-        else{
-          if(codeStart==false) {          // 코드 시작일 경우.
-            codeId = "code-result"+chasu;
-            $("#resultText").append('<pre><code id="'+codeId+'"></code></pre>');
-            hljs.highlightAll();
-            $("#"+codeId).removeClass("language-undefined");
-            codeStart = true;
-          }
-          else if(codeStart==true) {      // 코드 끝일 경우.
-            codeStart = false;
-            codeIndex = 0;
-          }
-          //console.log('highlight : '+codeStart);
-          bfCode = val[col];
+        if(codeStart==false) {          // 코드 시작일 경우.
+          codeId = "code-result"+chasu;
+          $("#resultText").append('<pre><code id="'+codeId+'"></code></pre>');
+          hljs.highlightAll();
+          textVal = "";             // 코드 내용 초기화.
+          $("#"+codeId).removeClass("language-undefined");
+          codeStart = true;
           chasu++;
         }
-  
+        else if(codeStart==true) {      // 코드 끝일 경우.
+          codeStart = false;
+          codeIndex = 0;
+        }
+        //console.log('highlight : '+codeStart);
+        bfCode = val[col];
+      }
+      else if($.trim(val[col]) == "`") {
+        if(bfCode == "``") {     // ``` 표시가 잘려서 오는 경우. 뒷부분 처리. 
+          console.log('bfCode = '+bfCode);
+          bfCode = "";
+        }
+        else{               // 코드시작이 아니라 문자의 따옴표인 경우.
+          //let accent = $.trim(val[col]).replace("`", "'");
+          $("#resultText").append(val[col]+cursorHtml); 
+        }
       }
       else if(codeStart==true) {        // 코드 내용 처리.
         if(codeIndex == 0) {            // 언어를 체크하기 위하여. 
@@ -200,19 +269,40 @@ async function printResponse(arrResult) {
           if(languageArr.includes(lang)) {        // 선언된 언어에 포함이 된다면 해당 값으로 세팅.
             codeLang = val[col];
             $("#"+codeId).addClass("language-"+codeLang);       // highlight language 세팅
+
+            //hljs.registerLanguage('javascript', e);
           }
           else{                                   // 기본값으로 자바스크립트로 세팅.
-            codeLang = "javascript";        // 기본적으로 자바스크립트 언어.
-            let highValue = hljs.highlight(val[col],  { language: codeLang }).value;
+            //console.log($("#hide_result").text());
+            //var inputLang = isIncludeLanguage(promptInput.value);
+            //var inputLang = isIncludeLanguage($("#hide_result").text());
+            //if(codeLang == "") {
+              var inputLang = isIncludeLanguage(promptInput.value);
+              console.log('language >>> '+inputLang);
+              if(!inputLang) {
+                inputLang = isIncludeLanguage($("#hide_result").text());
+                console.log('language >>> '+inputLang);
+              }
+              
+              codeLang = (inputLang)? inputLang:"javascript";        // 기본적으로 자바스크립트 언어.
+            //}
+            textVal = textVal + val[col];
+            let highValue = hljs.highlight(textVal,  { language: codeLang }).value;
             $("#"+codeId).addClass("language-"+codeLang);
-            $("#"+codeId).append(highValue+cursorHtml); 
+            $("#"+codeId).html(highValue+cursorHtml); 
+
+            //hljs.registerLanguage('javascript', e);
           }
           //console.log('language : '+codeLang);
         }
         else{
-          let highValue = hljs.highlight(val[col],  { language: codeLang }).value;
-          $("#"+codeId).append(highValue+cursorHtml); 
+          textVal = textVal + val[col];
+          let highValue = hljs.highlight(textVal,  { language: codeLang }).value;
+          //$("#"+codeId).append(highValue+cursorHtml); 
+          $("#"+codeId).html(highValue+cursorHtml);       // 코드 작성시는 .html() 메소드 이용 -> highlight 적용이 잘된다.
         }
+
+        //console.log('codeLang : '+codeLang);
         codeIndex++;
       }
       else{                 // 코드가 아닌 경우. 
